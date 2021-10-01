8be7bad83ef99d2bc3e8fc2a9ac1e93e5502207e
Steve M Silva <silvasm516@aol.com>
2021-10-01 02:20:01 -0700

    Update urls.py

--- birthdayProbRep/bp/urls.py
+++ birthdayProbRep/bp/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', include('Prob.urls'))    
+    path('', include('Prob.urls'))    
 ]
 
